b6686240a29ca3fcbc4991cf6aeadb8d26a74b79
1712397 <nnkduyen1999@gmail.com>
2021-06-10 22:25:00 +0700

    property binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -4,11 +4,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: `courses`,
   template: `
-    <h1>{{getTitle()}}</h1>
+    <h1 [ngStyle]="getStyleObj()" [class]="withBorder ? 'with-border' : null">{{ getTitle() }}</h1>
     <ul>
       <li *ngFor="let course of getCourses()">{{ course }}</li>
     </ul>
-  `
+  `,
+  styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent {
   private title = `List courses`;
@@ -16,6 +17,17 @@ export class CoursesComponent {
 
   constructor(service: CoursesService) {
     this.courses = service.getCourses();
+  }
+
+  private styleObj = {
+    color: `red`,
+    background: `whitesmoke`
+  };
+
+  withBorder = true;
+
+  getStyleObj() {
+    return this.styleObj;
   }
 
   getTitle() {
